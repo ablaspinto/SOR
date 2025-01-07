5fef80e17ec254e67f756a67bf432c96213040b5
ablaspinto <ablaspinto@gmail.com>
2025-01-07 13:18:20 -0500

    rest of commits

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+# Prototype Smart Order Routing Backtesting Software Report
 
-# Prototype Smart Order Routing Backtesting Software
+
+
+### Install Dependencies, listed below 
+pip install -r requirements.txt
+contourpy==1.3.1
+cycler==0.12.1
+fonttools==4.55.3
+kiwisolver==1.4.5
+matplotlib==3.10.0
+numpy==2.2.1
+pillow==11.0.0
+pycairo==1.27.0
+pyparsing==3.1.2
+python-dateutil==2.9.0
+six==1.16.0
+
+
+
+### Approach 
+
+
+### Take aways 
+
+I should have used data that was older, in order 
+to see if the model would be able to be profitable over a greater period of time 
+
---
+++ utils/parse_data.py
@@ -0,0 +1,45 @@
+import csv
+from datetime import datetime
+
+
+# parsing historical data, to create real time simulator to test strategies
+def parse_historical_data(filename):
+    start = 0
+    with open(filename) as file:
+        csv_file = csv.reader(file)
+        next(csv_file)
+        for record in csv_file:
+            ts_event = record[0]
+            r_type = record[1]
+            publisher_id = record[2]
+            instrument_id = record[3]
+            open_price = record[4]
+            high = record[5]
+            low = record[6]
+            close = record[7]
+            volume = record[8]
+            symbol = record[9]
+            if start == 0:
+                print("START", ts_event)
+                start = ts_event
+        end = convert_epoch_time(ts_event)
+        print(end)
+        start = convert_epoch_time(start)
+        print(start)
+        print(end - start)
+
+
+def convert_epoch_time(epoch):
+    """
+    function will convert the epoch string to time, which will then
+    be used to simulate a real time market
+    """
+    date = datetime.fromisoformat(epoch)
+    return date
+
+
+def main():
+    parse_historical_data("../data/processed/dbeq-basic-20241204.ohlcv-1s.csv")
+
+
+main()
